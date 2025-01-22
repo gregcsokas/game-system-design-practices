f6ab5a6767267148ac6574772973277fb168d74d
Gergő Csókás <gergo.csokas@gmail.com>
2025-01-22 15:08:07 +0700

    :fire: Remove unused `entity` from `deal_damage` system

--- simple_health_system_v2/src/damage/system.rs
+++ simple_health_system_v2/src/damage/system.rs
@@ -4,11 +4,11 @@ use crate::health::component::{Dead, Health};
 
 pub(crate) fn deal_damage(
     _commands: Commands,
-    mut query: Query<(Entity, &mut Health), Without<Dead>>,
+    mut query: Query<(&mut Health), Without<Dead>>,
     mut hit_event_reader: EventReader<HitEvent>
 ) {
     for hit in hit_event_reader.read() {
-        if let Ok((entity, mut health)) = query.get_mut(hit.target) {
+        if let Ok((mut health)) = query.get_mut(hit.target) {
             health.take_damage(hit.damage.get_damage());
             println!("Entity {:?} took {} damage", hit.target, hit.damage.get_damage());
         }
